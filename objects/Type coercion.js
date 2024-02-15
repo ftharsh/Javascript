e5b57127034941b4a04360e720d2323d7a1311de
Harshvardhan <107619247+ftharsh@users.noreply.github.com>
2024-02-15 10:18:54 +0530

    Create Type coercion.js

---
+++ objects/Type coercion.js
@@ -0,0 +1,14 @@
+let a = "33";
+let b = 44;
+
+// Console log the sum (concatenation)
+console.log(a + b); // Output: "3344"
+
+// Console log the data type of the sum
+console.log(typeof (a + b)); // Output: "string"
+
+// Console log the difference
+console.log(a - b); // Output: -11
+
+// Console log the data type of the difference
+console.log(typeof (a - b)); // Output: "number"
