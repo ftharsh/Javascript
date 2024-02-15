d38ac7167efe506a120958601bc242aaccf8c136
Harshvardhan <107619247+ftharsh@users.noreply.github.com>
2024-02-15 10:29:08 +0530

    typecoercion.js

---
+++ class 1/type coercion/typecoercion.js
@@ -0,0 +1,7 @@
+let a = "33";
+let b = 44;
+
+console.log(a + b); // Output: "3344"
+console.log(typeof (a + b)); // Output: "string"
+console.log(a - b); // Output: -11
+console.log(typeof (a - b)); // Output: "number"
